3168fcc00161e97c57182f870ebdb3fc6d3e2300
Balandzin_Anton <balandzin2602@gmail.com>
2023-09-18 22:35:55 +0300

    дlesson is over

--- Lessons/Lesson_1.playground/Contents.swift
+++ Lessons/Lesson_1.playground/Contents.swift
@@ -6,6 +6,24 @@ import UIKit
 // https://habr.com/ru/articles/761408/ - как установить на винду
 
 
+var greeting = "Hello, swiftbook"
+print(greeting)
 
+/*:
+ для маркдаун
+ Editor/Show Rendered Markup/ - для компиляции маркдауна
+ */
 
+// Свойство - объект, который хранит значение
 
+let сount = 123
+var newCount = сount
+newCount = 1234
+
+var userName: String? = nil
+userName = "Anton Balandzin"
+print(userName ?? "Tim Cook")
+
+let string = "aaa"
+var int = 123
+int = 1234
