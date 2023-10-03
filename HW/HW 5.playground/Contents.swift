260d4f7ef5c71b1d2e240b553f85f1d3f8a5d689
Balandzin_Anton <balandzin2602@gmail.com>
2023-10-03 17:02:49 +0300

    add all tasks

--- HW/HW 5.playground/Contents.swift
+++ HW/HW 5.playground/Contents.swift
@@ -51,24 +51,23 @@ print("result = \(result)")
  */
 let number = 9
 
-func forEvenParity(number: Int) -> Bool {
+func checkForParity(number: Int) -> Bool {
     number % 2 == 0
 }
 
-print(forEvenParity(number: number))
+print(checkForParity(number: number))
 /*:
 3.2 Создайте функцию, которая определяет делится ли число на *3* без остатка. Функция так же должна возвращать булево значение.
 */
-func dividedIntoThree(number: Int) -> Bool {
+func multipleOfThree(number: Int) -> Bool {
     number % 3 == 0
 }
 
-print(dividedIntoThree(number: number))
+print(multipleOfThree(number: number))
 /*:
  3.3 Создайте функцию, которая возвращает возрастающий массив чисел в интервале от *x* до *y*. Данный интервал должен задаваться при вызове функции при помощи параметров. Вы должны самостоятельно реализовать логику создания массива. Если хотите усложнить задание, то можете возвращать не сортированный массив чисел в заданном интервале. Главное, что бы числа были уникальными и не повторялись. При этом количество элементов массива должно соответствовать количеству элементов заданного интервала.
  */
 func getArray(from lowerLimit: Int, until upperLimit: Int) -> Set<Int> {
-    let count = upperLimit - lowerLimit + 1
     
     var numbers: Set<Int> = []
     
@@ -76,7 +75,7 @@ func getArray(from lowerLimit: Int, until upperLimit: Int) -> Set<Int> {
         numbers.insert(number)
     }
     
-    print("Множество размером \(count) элементов: \(numbers)")
+    print("Множество из \(numbers.count) элементов: \(numbers)")
     return numbers
 }
 
@@ -85,29 +84,41 @@ getArray(from: 6, until: 15)
 /*:
 3.4 Создайте массив чисел от *1* до *100*, используя для этого выше созданную функцию
  */
-getArray(from: 1, until: 100)
+let integers = getArray(from: 1, until: 100)
 /*:
  3.5 Создайте функцию для фильтрации переданного в неё массива. Функция должна возвращать новый массив без четных чисел. Для определения фильтруемых значений используйте ранее созданную функцию из задания **3.1**.
  */
-let integers = [1, 2, 3, 4, 5, 6, 7, 8, 8, 9, 10]
-
-func forEvenParity(numbers: [Int]) -> [Int] {
+func removeForEvenParity(numbers: Set<Int>) -> [Int] {
     var unevenNumbers: [Int] = []
     
     for number in numbers {
-        if !forEvenParity(number: number) {
+        if !checkForParity(number: number) {
             unevenNumbers.append(number)
         }
     }
     return unevenNumbers
 }
 
-forEvenParity(numbers: integers)
-
 /*:
  3.6 Создайте функцию для фильтрации переданного в неё массива. Функция должна возвращать новый массив без чисел кратных трем. Для определения фильтруемых значений используйте ранее созданную функцию из задания **3.2**.
  */
-
+func removeMultipleOfThree(numbers: [Int]) -> [Int] {
+    var unevenNumbers: [Int] = []
+    
+    for number in numbers {
+        if !multipleOfThree(number: number) {
+            unevenNumbers.append(number)
+        }
+    }
+    return unevenNumbers
+}
 /*:
  3.7 Отфильтруйте массив из задания **3.4** при помощи двух последний функций.
  */
+let numbersWithoutEven = removeForEvenParity(numbers: integers)
+
+let resultNumbers = removeMultipleOfThree(numbers: numbersWithoutEven)
+
+print("result array: \(resultNumbers)")
+
+
