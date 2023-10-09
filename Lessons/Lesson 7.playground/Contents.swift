67d47203b14b4ee52f3fe4c91587a61561ecd07c
Balandzin_Anton <balandzin2602@gmail.com>
2023-10-09 22:05:31 +0300

    end lesson

--- Lessons/Lesson 7.playground/Contents.swift
+++ Lessons/Lesson 7.playground/Contents.swift
@@ -2,4 +2,33 @@ import UIKit
 
 // Урок 7. ООП. Классы и объекты
 
+class Human {
+    var name: String
+    var age: Int
+        
+    var status: String {
+        if age >= 18 {
+            "Совершеннолетний"
+        } else if age < 18 && age >= 0 {
+            "Несовершеннолетний"
+        } else {
+            "Не установлено"
+        }
+    }
+    
+    func getInfo() {
+        print("name \(name) age \(age) status \(status)")
+    }
+    
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+    
+}
+
+let anton = Human(name: "Anton", age: 12)
+anton.status
+anton.getInfo()
+
 
