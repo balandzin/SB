d49906d1e0a71e52bdd01583ed3beabafc97dfba
Balandzin_Anton <balandzin2602@gmail.com>
2023-10-16 20:42:51 +0300

    enum

--- Lessons/Lesson_9.playground/Contents.swift
+++ Lessons/Lesson_9.playground/Contents.swift
@@ -2,3 +2,40 @@ import UIKit
 
 // Урок 9. Перечисления
 
+/*
+ - Синтаксис перечислений
+     - Возможность перечислений
+     - Исходные значения кейсов перечислений
+     - Инициализация
+     - Связанные значения (ассоциированные параметры)
+*/
+
+enum WeekDay {
+    case monday
+    case tuesday
+    case wednesday
+    case thursday
+    case friday
+    case saturday
+    case sunday
+}
+
+
+let weekday: WeekDay = .friday
+
+func setupAlarm(for weekday: WeekDay) {
+    switch weekday {
+    case .monday, .tuesday:
+        print("8 am")
+    case .wednesday:
+        print("8.30 am")
+    case .thursday:
+        print("9 am")
+    case .friday:
+        print("10 am")
+    default:
+        print("11 am")
+    }
+}
+
+
